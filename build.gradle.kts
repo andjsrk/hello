3bc55ee7f4a4d094f0b4b25f30aeda7684ac71bd
andjsrk <andjsrk0213@gmail.com>
2022-12-18 22:51:19 +0900

    add feature that is using paper api

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,11 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/io/github/andjsrk/hello/Hello.kt
+++ src/main/kotlin/io/github/andjsrk/hello/Hello.kt
@@ -1,5 +1,15 @@
 package io.github.andjsrk.hello
 
+import net.kyori.adventure.text.Component
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
 class Hello {
     fun hello() = "hello"
+    fun helloItem() =
+        ItemStack(Material.STICK).apply {
+            itemMeta = itemMeta.apply {
+                displayName(Component.text("hello"))
+            }
+        }
 }
